990afd89bc2030d9488473deade84e73da830e13
Joás <cabraljrjoas@tutanota.com>
2025-11-05 14:14:35 -0300

    gum, another lsp

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,4 +1,4 @@
-vim.lsp.enable({'lua_ls', 'gopls', 'rust_analyzer', 'ruff'})
+vim.lsp.enable({'lua_ls', 'gopls', 'rust_analyzer', 'ruff', 'ruby_lsp'})
 
 vim.api.nvim_create_autocmd(
   'LspAttach',
